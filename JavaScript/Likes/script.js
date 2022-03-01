6299c7cdd4e7864241d3b63dfb52e9c32415f8d1
DJC-00 <djcastle2172@gmail.com>
2022-03-01 11:55:51 -0600

    Likes 1.1

--- JavaScript/Likes/script.js
+++ JavaScript/Likes/script.js
@@ -3,20 +3,20 @@ var likeArr = [0,0,0]
 function likePost1(element){
     element.style.display = "block"
     likeArr[0] += 1;
-    element.innerText = (likeArr[0] + " Likes");
+    element.innerText = (likeArr[0] + " Like(s)");
     //console.log(this.innerText)
 }
 
 function likePost2(element){
     element.style.display = "block"
     likeArr[1] += 1;
-    element.innerText = (likeArr[1] + " Likes");
+    element.innerText = (likeArr[1] + " Like(s)");
     //console.log(this.innerText)
 }
 
 function likePost3(element){
     element.style.display = "block"
     likeArr[2] += 1;
-    element.innerText = (likeArr[2] + " Likes");
+    element.innerText = (likeArr[2] + " Like(s)");
     //console.log(this.innerText)
 }
